e2748a1692bc5378558aa8d75104e770f64e495c
patrickpowers848@gmail.com <patrickpowers848@gmail.com>
2017-04-23 06:03:57 -0400

    update

--- server.js
+++ server.js
@@ -9,9 +9,12 @@ var jsonParser = bodyParser.json();
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
+app.use(express.static('./app/public'))
+
+app.use('/static', express.static('./node_modules/font-awesome'))
 // parse various different custom JSON types as JSON 
 app.use(bodyParser.json({ type: 'application/*+json' }))
- 
+
 // parse some custom thing into a Buffer 
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
  
